de48aafa4c1916e9e36f4e66fcba336b8ed4525d
ST-Ncia <67141033+ST-Ncia@users.noreply.github.com>
2020-06-22 12:14:49 +0800

    new laravel skeleton with o2auth

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'HomeController@welcome');
+Route::get('/signin', 'AuthController@signin');
+Route::get('/callback', 'AuthController@callback');
+Route::get('/signout', 'AuthController@signout');
+/*
+Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signUp', function () {
+    return view('signUp');
+});
